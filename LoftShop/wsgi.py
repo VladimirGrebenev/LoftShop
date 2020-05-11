8939c81ffe131465de775bec0434190fe8ae9201
Vladimir Grebenev <grebenev-81@mail.ru>
2020-05-11 11:30:34 +0800

    first commit

---
+++ LoftShop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for LoftShop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LoftShop.settings')
+
+application = get_wsgi_application()
