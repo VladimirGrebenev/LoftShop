f00750af77fbb9a4fec6014d6ac30fb0baf516bf
Vladimir Grebenev <grebenev-81@mail.ru>
2020-05-20 13:24:01 +0800

    lesson5 basket

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.utils import timezone
 
+from basketapp.models import Basket
+
 from .models import Product, ProductCategory, Contact
 
 
@@ -9,16 +11,32 @@ from .models import Product, ProductCategory, Contact
 
 def main(request):
     title = "главная"
+    links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
 
     products = Product.objects.all()[:4]
 
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL, "basket": basket, "links_menu": links_menu,}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
     title = "товары"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by("price")
@@ -32,6 +50,7 @@ def products(request, pk=None):
             "category": category,
             "products": products,
             "media_url": settings.MEDIA_URL,
+            "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
 
@@ -41,6 +60,7 @@ def products(request, pk=None):
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "basket": basket,
     }
 
     if pk:
@@ -50,7 +70,16 @@ def products(request, pk=None):
 
 def contact(request):
     title = "о нас"
+    links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
     visit_date = timezone.now()
     locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    content = {"title": title, "visit_date": visit_date, "locations": locations, "basket": basket, "links_menu": links_menu,}
     return render(request, "mainapp/contact.html", content)
